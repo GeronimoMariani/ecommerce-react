91516ad9fb88909c5f178a428b3af30926ca72d7
Geronimo Mariani <geronimomariani5@gmail.com>
2023-07-19 15:16:32 -0300

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li className="1">HOLA</li>
+                <li className="2">CHAU</li>
+                <li className="3">HELLO</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
